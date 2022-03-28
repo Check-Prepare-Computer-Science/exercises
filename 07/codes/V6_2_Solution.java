d43aa85465f2ee964419105cab4ab052202b8cbe
nyanyan <nhanhuynh_online@protonmail.com>
2022-03-28 20:17:01 +0200

    Fix undefined delta variable

--- 07/codes/V6_2_Solution.java
+++ 07/codes/V6_2_Solution.java
@@ -1,14 +1,16 @@
+private static final double DELTA = 0.00001;
+
 @Test
 public void testTtiangleArea01() {
-  assertEquals(43.301, g.triangleArea(10, 10, 10), delta);
+  assertEquals(43.301, g.triangleArea(10, 10, 10), DELTA);
 }
 
 @Test
 public void testTtiangleArea02() {
-  assertEquals(4452.572, g.triangleArea(42, 221, 213), delta);
+  assertEquals(4452.572, g.triangleArea(42, 221, 213), DELTA);
 }
 
 @Test
 public void testTtiangleArea03() {
-  assertEquals(3901146.591, g.triangleArea(4324, 3242, 2432), delta);
+  assertEquals(3901146.591, g.triangleArea(4324, 3242, 2432), DELTA);
 }
